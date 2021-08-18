7a0413336a83c8cd211784d8947d180ad3815a8a
Genilton Souza <genilton.souza26@gmail.com>
2021-08-18 14:56:31 -0400

    Creating typescript models

---
+++ ng-video-game-db/src/app/models.ts
@@ -0,0 +1,49 @@
+export interface Game {
+  background_image: string;
+  name: string;
+  released: string;
+  metacritic_url: string;
+  website: string;
+  description: string;
+  metacritic: string;
+  genres: Array<Genre>;
+  parent_platforms: Array<ParentPlatform>;
+  publishers: Array<Publishers>;
+  ratings: Array<Rating>;
+  screenshots: Array<Screenshot>;
+  trailers: Array<Trailer>;
+}
+
+export interface APIResponse<T> {
+  results: Array<T>;
+}
+
+export interface Genre {
+  name: string;
+}
+
+export interface ParentPlatform {
+  platform: {
+    name: string;
+  };
+}
+
+export interface Publishers {
+  name: string;
+}
+
+export interface Rating {
+  id: number;
+  count: number;
+  title: string;
+}
+
+export interface Screenshot {
+  image: string;
+}
+
+export interface Trailer {
+  data: {
+    max: string;
+  };
+}
